1f298444b50fc7eb9ae35f9311a38087059e2263
MANBIR SINGH <163514635+ManbirS07@users.noreply.github.com>
2025-09-12 01:00:24 +0530

    Time: 15 ms (43.03%), Space: 21.4 MB (18.73%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -17,7 +17,24 @@ public:
                 if (i > 0) ways = (ways + dp[step - 1][i - 1]) % mod;
                 //moving right
                 if (i < maxPos) ways = (ways + dp[step - 1][i + 1]) % mod;
-                //stay
+                //ye esse kaam kar rha h ki left right ko ulta kar dia h
+                //DP is not simulating forward moves.
+
+                // Instead, we’re asking:
+                // “How many ways could I have arrived at pos at step t?”
+
+                // So we look at all possible previous positions that could land us here in 1 move:
+
+                // If you were at pos before → stay.
+
+                // If you were at pos-1 before → moved right into pos.
+
+                // If you were at pos+1 before → moved left into pos.
+
+                // That’s why we check pos+1.
+
+                //So checking pos+1 is just saying:
+                // Maybe I was at the right side before, and I moved left to arrive here.”
                 ways = (ways + dp[step - 1][i]) % mod;
 
                 dp[step][i] = ways;
