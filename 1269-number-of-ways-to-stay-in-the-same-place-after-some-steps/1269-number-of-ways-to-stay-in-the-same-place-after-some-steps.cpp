bf8b190b0452fc6003077344ac7601a47b49ec22
MANBIR SINGH <163514635+ManbirS07@users.noreply.github.com>
2025-09-12 00:53:22 +0530

    Time: 15 ms (43.03%), Space: 21.5 MB (18.27%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    typedef long long ll;
     int mod = 1e9 + 7;
-    int solve(int i, int steps, int n, vector<vector<int>>& dp) {
-        if(steps == 0) return i == 0;
-
-        if(dp[i][steps] != -1) return dp[i][steps];
-
-        long long left = 0, right = 0, stay = 0;
-        if(i > 0) {
-            left = solve(i - 1, steps - 1, n, dp);
-        }  
-
-        if(i < n - 1) {
-            right = solve(i + 1, steps - 1, n, dp);
-        }
-
-        stay = solve(i, steps - 1, n, dp);
-
-        return dp[i][steps] = (left + right + stay) % mod;
-    }
     int numWays(int steps, int len) {
         int maxPos = min(steps, len);
-        vector<vector<int>> dp(maxPos + 1, vector<int>(steps + 1, -1));
-        return solve(0, steps, len, dp);
+        vector<vector<ll>> dp(steps + 1, vector<ll>(maxPos + 1, 0));
+        //dp[step][position] -> ways to reach position using step steps
+        dp[0][0] = 1;
+        // dp[1][0] = 1; //at position 0 with one step -> stay
+        // dp[1][1] = 1; //at position 1 with one step -> stay
+
+        for(int step = 1; step <= steps; step++) { //har step me mai position ke saath 3 changes kar skta hu
+            for(int i = 0; i < maxPos; i++) {
+                ll ways = 0;
+                //moving left
+                if (i > 0) ways = (ways + dp[step - 1][i - 1]) % mod;
+                //moving right
+                if (i < maxPos) ways = (ways + dp[step - 1][i + 1]) % mod;
+                //stay
+                ways = (ways + dp[step - 1][i]) % mod;
+
+                dp[step][i] = ways;
+            }
+        }
+        return dp[steps][0];   
     }
 };
