78481b153e234df4a1a75400354c604a0e7af4bb
MANBIR SINGH <163514635+ManbirS07@users.noreply.github.com>
2025-10-09 15:53:13 +0530

    Time: 11 ms (17.67%), Space: 18.6 MB (18.49%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    map<int,int> mp;
-    unordered_map<int,int> dp; 
-
+    map<int, int> mp;
+    unordered_map<int, int> dp;
     int solve(auto it) {
         if(it == mp.end()) return 0;
-        int num = it->first;
 
-        if(dp.count(num)) return dp[num];
+        if(dp.count(it->first)) return dp[it->first];
+        int take = 0, skip = 0;
+        skip = solve(next(it));
 
-        auto nextIt = it; 
-        ++nextIt;
-        int skip = solve(nextIt);
-
-        auto jumpIt = mp.upper_bound(num + 1); // first element >= num+2
-        int take = num * it->second + solve(jumpIt);
-
-        return dp[num] = max(skip, take);
+        int element = (*it).first;
+        int freq = (*it).second;
+        //jaise 3 pe hu, toh next I can go to 5 or higher, i.e first element >= nums[i] + 2 ya first element > nums[i] + 1
+        auto idx = mp.upper_bound(element + 1);
+        take = element * freq + solve(idx);
+        return dp[element] = max(take, skip);
     }
-
     int deleteAndEarn(vector<int>& nums) {
-        for(int num: nums) mp[num]++;
+        //isme esse h jaise
+        //if I delete 2, i would also have to delete 3 so next i would explore 4
+        //If i delete 3, i would have skipped till here in one choice and then I cannot explore 4
+        for(int x: nums) {
+            mp[x]++; //I dont care about duplicates, on deleting an element I will get it's freq * deleted element points, basically we can't take consecutive elements so we would have needed a sorted array without duplicates hence we take a map
+        }
+        int n = nums.size();
         return solve(mp.begin());
     }
 };
